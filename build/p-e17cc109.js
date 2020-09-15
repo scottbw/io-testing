762428a7e793487f836f59a7fca4032027425085
scottbw <scott.bradley.wilson@gmail.com>
2020-09-15 12:45:06 +0100

    candidate build

--- build/p-e17cc109.js
+++
@@ -1,1 +0,0 @@
-function t(t){let e=Number(t);return isNaN(e)?t:e%1==0?e.toFixed(0):r(t,2)}function r(t,r){let e;try{e=parseFloat(t)}catch(r){return t}return isNaN(e)?t:e.toFixed(r)}export{t as f,r as n}
